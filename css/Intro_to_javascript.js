d7bf584f932270f763d6761a9d80d8ebe2eb7dba
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-04-03 10:29:15 -0500

    inline javascript exercises

---
+++ css/Intro_to_javascript.js
@@ -0,0 +1,7 @@
+var offerStatus = true;
+var numberofitems = 3;
+var PremiumMember = true;
+
+if((memberStatus && offerStatus) || (numberofitems && offerStatus) || (numberofitems > 2 & offerStatus) {
+    console.log
+}
---
+++ js/external_js.js
@@ -0,0 +1,45 @@
+"use strict";
+
+console.log("Hello, from external JavaScript!");
+
+alert("Welcome to my website!");
+
+var userInput = prompt("What's your favorite color?");
+alert ("Oh, I suppose " + userInput + " is a nice color.");
+
+console.log("Wow! " + userInput + " is my favorite color, too!!");
+
+alert("WARNING!!! Your PC has be infected by RENT MOVIE VIRUS!  To clear virus, you must rent movies for your kids!!!!!");
+
+alert("You must rent 'The Little Mermaid'!!!");
+var Mermaid = prompt("How many day?!");
+console.log(Mermaid + " day!");
+
+alert("You must rent 'Brother Bear'!!!!!!");
+var Bear = prompt("How many day?!");
+console.log(Bear + " day!!");
+
+alert("You must rent 'Hercules'!!!!!!!!!!!");
+var Hercules = prompt("How many day??!!");
+console.log(Hercules + " day!!!");
+
+var Hercules = parseInt(Hercules);
+var Mermaid = parseInt(Mermaid);
+var Bear = parseInt(Bear);
+
+var Extortion = (3 * (Hercules + Mermaid + Bear));
+console.log("The cost is $" + Extortion + "!!!!!!");
+
+alert(("$3 per day!! You will be charge $") + (Extortion) + ("!!!!!!" ));
+console.log("Muahahahahahahahaha!!!");
+
+var CreditCard = prompt("Put Credit Card Number here!!  Or FBI come for you kids!!");
+console.log(CreditCard);
+var Exp = prompt("Expiration date?");
+console.log(Exp);
+var Security = prompt("Security code?");
+console.log(Security);
+
+alert("Thank you!  Enjoy your movies! :)");
+
+alert("By the way, you shouldn't be so trusting.  Here is my new boat!");
